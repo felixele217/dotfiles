527a2ed0c28bb4eefa4fce281a012701a943d7c0
felixele217 <felix.droese@gmx.net>
2023-03-02 14:29:03 +0100

    added a lot of basic setups

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+alias a='[ -f sail ] && sh sail || sh vendor/bin/sail artisan'
+
+alias at='php artisan test'
+
+alias mfs='[ -f sail ] && sh sail || sh vendor/bin/sail artisan migrate:fresh --seed'
+
+alias routes='[ -f sail ] && sh sail || sh vendor/bin/sail artisan route:list'
+alias dev='npm run dev'
+
