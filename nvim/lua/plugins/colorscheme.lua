bf87915c1640101bc91326d07955f8b7d4657987
felixele217 <droese.felix@gmail.com>
2024-09-16 13:47:19 +0200

    more updates

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,15 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin" },
   { "navarasu/onedark.nvim" },
+  { "folke/tokyonight.nvim" },
 
   -- Configure LazyVim to load One Dark
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "onedark",
+      style = "warm",
+      term_colors = true,
     },
   },
 }
