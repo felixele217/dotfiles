f104c29cc6057390942482f3d4064093031d4e21
felixele217 <droese.felix@gmail.com>
2024-09-24 09:04:45 +0200

    Revert "update"
    This reverts commit e18dbe0d0b5a9db71831cb049e72b813ab847dfc.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>tn", function()
-  require("neotest").run.run()
-end)
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
