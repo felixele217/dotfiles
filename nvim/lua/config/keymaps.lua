cb0e3d3ac6b6be767bef5d1c62a09714c005b761
felix-droese <felix.droese@clockin.de>
2024-09-24 13:36:48 +0200

    readd install script and new theme

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
-vim.keymap.set("n", "<leader>tn", function()
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>tn", function()
   require("neotest").run.run()
 end)
+
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,11 @@ return {
   { "navarasu/onedark.nvim" },
   { "folke/tokyonight.nvim" },
 
-  -- Configure LazyVim to load One Dark
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
-      style = "warm",
+      colorscheme = "tokyonight",
+      -- style = "warm",
     },
   },
 }
