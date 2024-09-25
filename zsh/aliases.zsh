44f79473ce6b8620c246de1908dd4bcc8770530e
felix-droese <felix.droese@clockin.de>
2024-09-25 12:26:36 +0200

    add new alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias phpcs='./vendor/bin/php-cs-fixer fix --config=.php-cs-fixer.dist.php -v'
 alias ptest='php artisan test --parallel'
 
 alias duskserve='php artisan serve --env=testing --port 8008'
+alias duskdriver='sudo ./vendor/laravel/dusk/bin/chromedriver-mac-arm --port=9515'
 alias dusktest='php artisan dusk --env=testing'
 
 alias routes='php artisan routes:list'
