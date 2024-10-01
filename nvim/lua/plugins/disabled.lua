96bfa34e3076274572050f60ed375d8a35a99d27
felix-droese <felix.droese@clockin.de>
2024-10-01 15:27:32 +0200

    update

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
-  { "folke/noice.nvim", enabled = false },
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    { "folke/noice.nvim",            enabled = false },
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    { "nvim-neotest/neotest",        enabled = false },
+    { "akinsho/bufferline.nvim",     enabled = false },
 }
--- nvim/lua/plugins/vim-test.lua
+++ nvim/lua/plugins/vim-test.lua
@@ -4,10 +4,11 @@ return {
         "preservim/vimux"
     },
     config = function()
-        vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", {})
-        vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", {})
+        vim.keymap.set("n", "<leader>tt", ":silent TestNearest<CR>", {})
+        vim.keymap.set("n", "<leader>tf", ":silent TestFile<CR>", {})
+        vim.keymap.set("n", "<leader>tl", ":silent TestLast<CR>", {})
+
         -- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
-        vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
         -- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
         vim.cmd("let test#strategy = 'vimux'")
         vim.cmd("let test#php#phpunit#executable = 'php artisan test'")
