72c162615ce3c04dbb258e47ba52e85f20554c92
felix-droese <felix.droese@clockin.de>
2024-10-02 09:57:36 +0200

    update conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,39 +1,30 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        config = function()
-            local conform = require("conform")
-
-            conform.setup({
-                formatters_by_ft = {
-                    blade = { "blade-formatter" },
-                    vue = { "prettier" }
-                },
-                format_on_save = {
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 1000,
-                },
-                notify_on_error = true,
-                formatters = {
-                    php = {
-                        command = "php-cs-fixer",
-                        args = {
-                            "fix",
-                            "$FILENAME",
-                            "--config=./vendor/bin/php-cs-fixer fix.php --config=.php-cs-fixer.dist.php -v",
-                        },
-                        stdin = false,
-                    },
-                },
-            })
-        end,
-        keys = {
-            {
-                "<leader>cf",
-                ":lua require('conform').format()<CR>",
-                desc = "Format Current File",
-            },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        blade = { "blade-formatter" },
+        vue = { "prettier" },
+      },
+      notify_on_error = true,
+      formatters = {
+        php = {
+          command = "php-cs-fixer",
+          args = {
+            "fix",
+            "$FILENAME",
+            "--config=./vendor/bin/php-cs-fixer fix.php --config=.php-cs-fixer.dist.php -v",
+          },
+          stdin = false,
         },
+      },
     },
+    keys = {
+      {
+        "<leader>cf",
+        ":lua require('conform').format()<CR>",
+        desc = "Format Current File",
+      },
+    },
+  },
 }
