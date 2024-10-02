ce671f8af821fea03e1cb558abb7dbc8ed5d3753
felix-droese <felix.droese@clockin.de>
2024-10-02 11:14:43 +0200

    color column

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,15 +1,18 @@
 return {
-    "hrsh7th/nvim-cmp",
+  "hrsh7th/nvim-cmp",
+  dependencies = { "adoolaard/nvim-cmp-laravel" },
 
-    opts = function(_, opts)
-        local cmp = require("cmp")
+  opts = function(_, opts)
+    local cmp = require("cmp")
 
-        opts.mapping = vim.tbl_extend("force", opts.mapping, {
-            ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-            ["<CR>"] = function(fallback)
-                cmp.abort()
-                fallback()
-            end,
-        })
-    end,
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      ["<CR>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end,
+    })
+
+    require("nvim-cmp-laravel").setup()
+  end,
 }
