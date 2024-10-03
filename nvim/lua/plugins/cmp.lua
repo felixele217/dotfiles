adb8b8eeea904ff12ec72e7ba15e6d4da76917fe
felix-droese <felix.droese@clockin.de>
2024-10-03 19:04:17 +0200

    it works

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -34,9 +34,14 @@ return {
       { name = "path" },
     }
 
+    local function ltrim(s)
+      return s:match("^%s*(.*)")
+    end
+
     opts.formatting = {
+      fields = { "kind", "abbr", "menu" },
       format = lspkind.cmp_format({
-        mode = "symbol_text",
+        mode = "symbol",
         maxwidth = 50,
         menu = {
           nvim_lsp = "[LSP]",
@@ -46,23 +51,10 @@ return {
           path = "[Path]",
         },
         before = function(entry, vim_item)
-          vim_item.menu = vim_item.kind .. " (" .. entry.source.name .. ")"
+          vim_item.menu = "  " .. vim_item.kind .. " (" .. entry.source.name .. ")"
           vim_item.menu_hl_group = "SpecialComment"
 
-          if vim_item.kind == "Color" and entry.completion_item.documentation then
-            local _, _, r, g, b = string.find(entry.completion_item.documentation, "^rgb%((%d+), (%d+), (%d+)")
-            if r then
-              local color = string.format("%02x", r) .. string.format("%02x", g) .. string.format("%02x", b)
-              local group = "Tw_" .. color
-              if vim.fn.hlID(group) < 1 then
-                vim.api.nvim_set_hl(0, group, { fg = "#" .. color })
-              end
-              vim_item.kind_hl_group = group
-              return vim_item
-            end
-          end -- or just show the icon
-          -- vim_item.kind = icons[vim_item.kind] and icons[vim_item.kind] or vim_item.kind
-          vim_item.kind = lspkind.symbolic(vim_item.kind) and lspkind.symbolic(vim_item.kind) or vim_item.kind
+          vim_item.abbr = ltrim(vim_item.abbr)
 
           return vim_item
         end,
