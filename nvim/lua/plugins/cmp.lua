950b09e20656e9bbf58462a1d818b2f27c4bad5a
felix-droese <felix.droese@clockin.de>
2024-12-22 14:28:53 +0100

    wezterm and cmp update

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -21,13 +21,13 @@ return {
         -- })
 
         opts.mapping = vim.tbl_extend("force", opts.mapping, {
-            ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+            -- ["<Tab>"] = cmp.mapping.confirm({ select = true }),
             ["<CR>"] = function(fallback)
                 cmp.abort()
                 fallback()
             end,
-            ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-            ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+            -- ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+            -- ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
         })
 
         opts.experimental = {
