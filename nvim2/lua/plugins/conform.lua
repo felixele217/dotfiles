4f9ce3a4d85fd4e9840944021ab717a3ea71b7ff
felix-droese <felix.droese@clockin.de>
2024-12-23 09:42:38 +0100

    wip kickstart as main

---
+++ nvim2/lua/plugins/conform.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                blade = { "blade-formatter" },
+                vue = { "prettier" },
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                php = { "phpcsfixer", 'pint' },
+                lua = { "styleua " },
+            },
+            notify_on_error = true,
+            formatters = {
+                phpcsfixer = {
+                    command = "php-cs-fixer",
+                    args = {
+                        "fix",
+                        "$FILENAME",
+                        "--config=.php-cs-fixer.dist.php",
+                        "-v",
+                    },
+                    stdin = false,
+                },
+            },
+        },
+        keys = {
+            {
+                "<leader>cf",
+                ":lua require('conform').format()<CR>",
+                desc = "Format Current File",
+            },
+        },
+    },
+}
