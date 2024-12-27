2b6bd9996419bd36c1f802311ee13eacdddbe30f
felix-droese <felix.droese@clockin.de>
2024-12-27 09:48:16 +0100

    wip

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -31,7 +31,7 @@ return {
         'typescript',
         'vim',
         'vimdoc',
-        'vue'
+        'vue',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -40,23 +40,41 @@ return {
       },
       indent = { enable = true, disable = { 'yaml' } },
     },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+
+      local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+      parser_config.blade = {
+        install_info = {
+          url = 'https://github.com/EmranMR/tree-sitter-blade',
+          files = { 'src/parser.c' },
+          branch = 'main',
+        },
+        filetype = 'blade',
+      }
+      vim.filetype.add {
+        pattern = {
+          ['.*%.blade%.php'] = 'blade',
+        },
+      }
+    end,
   },
   { -- Treesitter context
     'nvim-treesitter/nvim-treesitter-context',
     opts = {
-      enable = true,           -- Enable this plugin (Can be enabled/disabled later via commands)
-      max_lines = 0,           -- How many lines the window should span. Values <= 0 mean no limit.
-      min_window_height = 0,   -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
       line_numbers = true,
       multiline_threshold = 3, -- Maximum number of lines to show for a single context
-      trim_scope = 'outer',    -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = 'cursor',         -- Line used to calculate context. Choices: 'cursor', 'topline'
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
       -- Separator between context and content. Should be a single character string, like '-'.
       -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
       separator = nil,
-      zindex = 20,     -- The Z-index of the context window
+      zindex = 20, -- The Z-index of the context window
       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-    }
-  }
+    },
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
