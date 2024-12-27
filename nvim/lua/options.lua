f25f18e66fc99498140852d79076b7193ee41768
felix-droese <felix.droese@clockin.de>
2024-12-27 23:22:45 +0100

    fix breaking change

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -50,7 +50,6 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = ' ', trail = ' ', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
